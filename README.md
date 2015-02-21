8baf2fb977cf0255fea7398bb9b2ac0ca5cffcf5
Matthew Gross <contact@mattgross.net>
2015-02-21 14:41:37 -0500

    changes to send mail script

--- README.md
+++ README.md
@@ -2,8 +2,5 @@
 Our pimp homepage.
 
 #TODO:
-- File needs to be cleaned up
 - Fixing of scrolling error / bug. Page should scroll like a slideshow.
 - Lego images need to be added.
-- Platform section needs updating
-- Contact form script (matt)
--- send-email.php
+++ send-email.php
@@ -1,17 +1,23 @@
 <?php
 
 $receiving_email = 'ibelieveicanfly@pigeolabs.com'; // standard contact email...
+$formmailer_email = 'formmailer@pigeolabs.com';
 
 // DO NOT TOUCH BELOW UNLESS YOU KNOW WHAT YOU ARE DOING!!!
 
 function send_mail($name, $email, $message) {
+	global $receiving_email, $formmailer_email;
+
 	$subject = 'eMail from '.$name.' using Contact Form!';
 	$message = '<h2>You have received the following message from '.$name.':</h2><br /><br /><p>'.$message.'</p>';
-	$headers = 'From: ' . 'form-mailer@pigeolabs.com' . "\r\n" .
+	$headers = 'From: ' . $formmailer_email . "\r\n" .
     	'Reply-To: '. $email . "\r\n" .
     	'X-Mailer: PHP/' . phpversion();
 
-	mail($receiving_email, $subject, $message, $headers);
+	if(mail($receiving_email, $subject, $message, $headers))
+		return true;
+		
+	return false; // by default.
 }
 	
 $method = $_SERVER['REQUEST_METHOD'];
@@ -22,14 +28,14 @@ if($method === "POST") {
 
 	if(isset($name) && $name !==  '' && isset($email) && $email !== '' && isset($message) && $message !== '') {
 		if(send_mail($name, $email, $message)) {
-			header('Location: /?form_success=true');
+			header('Location: /?form_success=true#contact');
 		}
 		else {
-			header('Location: /?form_failed=true&message=send_failure');
+			header('Location: /?form_failed=true&message=send_failure#contact');
 		}
 	}
 	else {
-		header('Location: /?form_failed=true&message=missing_info');
+		header('Location: /?form_failed=true&message=missing_info#contact');
 	}
 }
 else {
